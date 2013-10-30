3d8f2a2e61c79f050e8a051ebcc6ffa4fe743ffa
Chi-En Wu <jason2506.mail@gmail.com>
2013-10-30 14:49:42 +0800

    pyramid() should be private

--- phod.m
+++ phod.m
@@ -21,3 +21,38 @@ sumX = sum(x);
 if sumX ~= 0
     x = x / sumX;
 end
+
+% =========================
+%   Private Function(s)
+% =========================
+
+function [x] = pyramid(G, B, K, L)
+
+m = size(G, 1);
+n = size(G, 2);
+
+x = [];
+if L > 0
+    % divide image into 4 blocks
+    bm = ceil(m / 2);
+    bn = ceil(n / 2);
+    colG = mat2cell(G, [bm, m - bm], [bn, n - bn]);
+    colB = mat2cell(B, [bm, m - bm], [bn, n - bn]);
+
+    % compute orientation histograms for each block
+    totalX = zeros(K, 1);
+    for idx = 1:4
+        blockG = colG{idx};
+        blockB = colB{idx};
+
+        blockX = pyramid(blockG, blockB, K, L - 1);
+        totalX = totalX + blockX(1:K);
+        x = [x; blockX];
+    end
+
+    x = [totalX; x];
+else
+    for k = 1:K
+        x = [x; sum(G(B == k))];
+    end
+end
